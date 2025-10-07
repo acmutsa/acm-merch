714cfef1d3a5d089d29912bda867d844b1560419
joshuasilva414 <joshuasilva414@gmail.com>
2025-10-06 22:25:00 -0500

    add betterAuth

--- db/index.ts
+++ db/index.ts
@@ -1,9 +1,14 @@
-import "dotenv/config";
+import { config } from "dotenv";
 import { drizzle } from "drizzle-orm/libsql";
+import * as schema from "./schema";
+import * as betterAuthSchema from "./auth-schema";
+
+config({ path: "../.env" });
 
 export const db = drizzle({
   connection: {
-    url: process.env.DATABASE_URL!,
-    authToken: process.env.DATABASE_AUTH_TOKEN!,
+    url: process.env.TURSO_DATABASE_URL!,
+    authToken: process.env.TURSO_AUTH_TOKEN!,
   },
+  schema: { ...schema, ...betterAuthSchema },
 });
