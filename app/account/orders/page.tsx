f5646a972e618af25c35cd12cd82a52fb1f9210d
AlessandroEspinosa2003 <alessandroespinosa2003@gmail.com>
2025-10-12 17:59:22 -0500

    Set up Account Routes

---
+++ app/account/orders/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+    return (
+      <div>
+        <h1>Account/Orders</h1>
+      </div>
+    );
+  }
---
+++ app/account/settings/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+    return (
+      <div>
+        <h1>Account/Settings</h1>
+      </div>
+    );
+  }
