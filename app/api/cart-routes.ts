9ee6fce3573c536a7024666b7b776bd297c744f2
AlessandroEspinosa2003 <alessandroespinosa2003@gmail.com>
2025-11-18 00:48:31 -0600

    Shopping Cart Cookie and Cart Cookie API Framework

---
+++ app/api/cart-routes.ts
@@ -0,0 +1,77 @@
+import {NextResponse} from "next/server";
+import {
+    CartItem,
+    getCartFromCookie,
+    saveCartToCookie,
+} from "@/lib/cart-cookies";
+
+export async function GET() {
+    const cart = getCartFromCookie();
+    return NextResponse.json(cart);
+}
+
+export async function POST(req:Request) {
+    const body = (await req.json()) as { id: string; productName: string; price: number; size?: string; quantity?: number; };
+    const qty = body.quantity ?? 1;
+
+    let cart = getCartFromCookie();
+    const index = cart.findIndex((item) => item.id === body.id && item.size === body.size);
+
+    if (index >= 0) {
+        const existing = cart[index];
+        const updated: CartItem = {
+            ...existing,
+            quantity: existing.quantity + qty;
+        };
+        cart[index] = updated;
+    } else {
+        const newItem: CartItem = {
+            id: body.id,
+            productName: body.productName,
+            price: body.price,
+            size: body.size,
+            quantity: qty,
+        };
+        cart.push(newItem);
+    }
+
+    saveCartToCookie(cart);
+    return NextResponse.json(cart, {status: 200});
+}
+
+export async function PUT(req: Request) {
+    const body = (await req.json() as { id: string; size?: string; quantity: number; });
+
+    let cart = getCartFromCookie();
+
+    if (body.quantity <= 0) {
+        cart = cart.filter(
+            (item) => !(item.id === body.id && item.size === body.size)
+          );
+          saveCartToCookie(cart);
+          return NextResponse.json(cart, { status: 200 });
+    }
+    cart = cart.map((item) =>
+        item.id === body.id && item.size === body.size
+          ? { ...item, quantity: body.quantity }
+          : item
+      );
+    
+      saveCartToCookie(cart);
+      return NextResponse.json(cart, { status: 200 });    
+}
+
+export async function DELETE(req: Request) {
+    const body = (await req.json()) as {
+      id: string;
+      size?: string;
+    };
+  
+    let cart = getCartFromCookie();
+    cart = cart.filter(
+      (item) => !(item.id === body.id && item.size === body.size)
+    );
+  
+    saveCartToCookie(cart);
+    return NextResponse.json(cart, { status: 200 });
+}
