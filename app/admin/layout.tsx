6b63ea99c503d4921389e99049fdbad4681e3114
joshuasilva414 <joshuasilva414@gmail.com>
2025-11-19 18:03:04 -0600

    fix admin sidebar

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,26 +1,9 @@
-import { Input } from "@/components/ui/input";
 import {
-  Select,
-  SelectValue,
-  SelectTrigger,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-} from "@/components/ui/select";
-import {
-  Sidebar,
-  SidebarContent,
-  SidebarGroup,
-  SidebarGroupContent,
-  SidebarGroupLabel,
-  SidebarMenuButton,
-  SidebarMenuItem,
   SidebarProvider,
   SidebarInset,
-  SidebarMenu,
 } from "@/components/ui/sidebar";
-import Link from "next/link";
 import { Suspense } from "react";
+import AdminSidebar from "./admin-sidebar";
 
 export default function AdminLayout({
   children,
@@ -29,32 +12,7 @@ export default function AdminLayout({
 }) {
   return (
     <SidebarProvider>
-      <Sidebar variant="inset" collapsible="none" className="h-full w-md">
-        <SidebarContent className="h-full">
-          <SidebarGroup>
-            <SidebarGroupLabel>Dashboard</SidebarGroupLabel>
-            <SidebarGroupContent>
-              <SidebarMenu>
-                <SidebarMenuItem>
-                  <SidebarMenuButton asChild>
-                    <Link href="/admin/products">Products</Link>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-                <SidebarMenuItem>
-                  <SidebarMenuButton asChild>
-                    <Link href="/admin/orders">Orders</Link>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-                <SidebarMenuItem>
-                  <SidebarMenuButton asChild>
-                    <Link href="/admin/users">Users</Link>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-              </SidebarMenu>
-            </SidebarGroupContent>
-          </SidebarGroup>
-        </SidebarContent>
-      </Sidebar>
+      <AdminSidebar />
       <Suspense fallback={<div>Loading...</div>}>
         <SidebarInset className="h-full">
           <div className="h-full w-full p-4">{children}</div>
